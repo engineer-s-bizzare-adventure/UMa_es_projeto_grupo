245741b6d04b88237ee01a85d8dff15561471834
Luís Aguiar <luisaguiar96@hotmail.com>
2021-06-22 00:22:35 +0100

    Created admin form, query for item history

---
+++ projeto-es/Business_Layer/ItemService.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+using Dapper;
+using projeto_es.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace projeto_es.Business_Layer
+{
+    public class ItemService
+    {
+        private readonly MySqlConnection _conn;
+        private string _connectionString = DbContext.GetMySQLConnectionString();
+
+        public ItemService()
+        {
+            _conn = new MySqlConnection(_connectionString);
+        }
+
+        public List<Item> CheckPrescribedItemHistory()
+        {
+            string fetchAllItems = "SELECT * FROM item";
+            var listOfItems = _conn.Query<Item>(fetchAllItems).ToList();
+
+            return listOfItems;
+        }
+    }
+}
